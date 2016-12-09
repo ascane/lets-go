c131fa251bd9679d1f960fa26b9e018e49d88b88
ascane <mandy00724@gmail.com>
2016-12-09 16:38:39 +0100

    Test the environment by forking env_runner.py, random_agent.py and uploader.py.

---
+++ go/env/random_agent.py
@@ -0,0 +1,57 @@
+import argparse
+import logging
+import sys
+
+import gym
+import env_runner
+
+# In modules, use `logger = logging.getLogger(__name__)`
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler(sys.stderr))
+
+class RandomAgent(object):
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    def act(self, observation, reward, done):
+        return self.action_space.sample()
+
+def complete(results):
+    return len(results['episode_lengths']) == 10
+
+def run_random(env):
+    episode_count = 10
+    agent = RandomAgent(env.action_space)
+
+    for i in xrange(episode_count):
+        ob = env.reset()
+        reward = done = None
+
+        while True:
+            action = agent.act(ob, reward, done)
+            ob, reward, done, _ = env.step(action)
+            if done:
+                break
+
+def always_true(id):
+    return True
+
+def main():
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('-b', '--base-dir', help='Set base dir.')
+    parser.add_argument('-v', '--verbose', action='count', dest='verbosity', default=0, help='Set verbosity.')
+    args = parser.parse_args()
+
+    if args.verbosity == 0:
+        logger.setLevel(logging.INFO)
+    elif args.verbosity >= 1:
+        logger.setLevel(logging.DEBUG)
+
+    runner = env_runner.EnvRunner('random-v3', run_random, complete, base_dir=args.base_dir, video_callable=always_true, env_ids=['Go9x9-v0'])
+#     runner = env_runner.EnvRunner('random-v3', run_random, complete, base_dir=args.base_dir, video_callable=always_true, env_ids=['CartPole-v0'])
+    runner.run()
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
--- go/test/test_openai.py
+++ go/test/test_openai.py
@@ -3,3 +3,21 @@ import gym
 env = gym.make('Go9x9-v0')
 env.reset()
 env.render()
+
+    
+for i_episode in range(1):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        print(env.action_space)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+
+# b.env.state.board.clone()
+# b.play_inplace(coord, color)
+# b.is_terminal()
+# b.official_score()

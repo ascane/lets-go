61a7430f27acdb5d0bbf134fd48b105e28956c45
ascane <mandy00724@gmail.com>
2016-12-11 13:30:11 +0100

    Test play_inplace(coord, color) in test_openai.py.

--- go/test/test_openai.py
+++ go/test/test_openai.py
@@ -5,19 +5,24 @@ env.reset()
 env.render()
 
     
-for i_episode in range(1):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        print(env.action_space)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+# for i_episode in range(1):
+#     observation = env.reset()
+#     for t in range(100):
+#         env.render()
+#         print(observation)
+#         print(env.action_space)
+#         action = env.action_space.sample()
+#         observation, reward, done, info = env.step(action)
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             break
 
-# b.env.state.board.clone()
+b = env.state.board.clone()
+b.play_inplace(19, 2)
+print(b)
+b.play_inplace(20, 1)
+print(b)
+b.play_inplace(31, 2)
+print(b)
 # b.play_inplace(coord, color)
-# b.is_terminal()
 # b.official_score()

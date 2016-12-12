1bde0f9d5d02ec6648587fa4be8208a7b752e7d9
ascane <mandy00724@gmail.com>
2016-12-12 19:44:44 +0100

    Test some other methods.

--- go/test/test_openai.py
+++ go/test/test_openai.py
@@ -4,25 +4,30 @@ env = gym.make('Go9x9-v0')
 env.reset()
 env.render()
 
-    
 # for i_episode in range(1):
 #     observation = env.reset()
 #     for t in range(100):
 #         env.render()
 #         print(observation)
-#         print(env.action_space)
 #         action = env.action_space.sample()
 #         observation, reward, done, info = env.step(action)
+#         print(reward)
 #         if done:
 #             print("Episode finished after {} timesteps".format(t+1))
 #             break
 
 b = env.state.board.clone()
-b.play_inplace(19, 2)
+print(b.official_score)
+b.play_inplace(19, 2) # coord, color
 print(b)
+print(b.official_score)
 b.play_inplace(20, 1)
 print(b)
+print(b.official_score)
 b.play_inplace(31, 2)
 print(b)
-# b.play_inplace(coord, color)
-# b.official_score()
+print(b.official_score)
+if b.is_terminal:
+    print("yes")
+else:
+    print("no")

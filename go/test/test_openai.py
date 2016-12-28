95360f68dd776ba54d3531ff364385fc2531c16a
ascane <mandy00724@gmail.com>
2016-12-28 10:56:13 +0100

    Test some methods of PyPachiBoard.

--- go/test/test_openai.py
+++ go/test/test_openai.py
@@ -1,4 +1,7 @@
 import gym
+
+BLACK = 1
+WHITE = 2
  
 env = gym.make('Go9x9-v0')
 env.reset()
@@ -18,16 +21,39 @@ env.render()
 
 b = env.state.board.clone()
 print(b.official_score)
-b.play_inplace(19, 2) # coord, color
+b.play_inplace(19, WHITE) # coord, color
 print(b)
 print(b.official_score)
-b.play_inplace(20, 1)
+b.play_inplace(20, BLACK)
 print(b)
 print(b.official_score)
-b.play_inplace(31, 2)
+b.play_inplace(31, WHITE)
+
 print(b)
 print(b.official_score)
 if b.is_terminal:
     print("yes")
 else:
     print("no")
+
+ 
+black_stone_array = b.get_stones(BLACK)
+white_stone_array = b.get_stones(WHITE)
+print(white_stone_array)
+
+print(b.black_stones)
+print(b.white_stones)
+
+print(b.size)
+
+print(b.get_legal_coords(BLACK, filter_suicides=True))
+print(b.coord_to_ij(20))
+print(b.ij_to_coord(8, 8))
+
+l = [5, 1, 2, 3]
+print(sorted(l)[-1])
+
+
+# The implemented methods of PachiPy: https://github.com/openai/pachi-py/blob/master/pachi_py/cypachi.pyx#L159
+# black (X) = 1
+# white (O) = 2
